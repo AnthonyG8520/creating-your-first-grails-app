083fdae510616eb0a1cb80edb1fc39357a182185
AnthonyG8520 <anthony.j.gonzales22@gmail.com>
2022-05-30 14:54:07 -0500

    setting up first grails project

---
+++ initial/grails-app/Services/org/grails/guides/MakeService.groovy
@@ -0,0 +1,5 @@
+package org.grails.guides
+
+interface MakeService {
+
+}
--- initial/grails-app/init/org/grails/guides/BootStrap.groovy
+++ initial/grails-app/init/org/grails/guides/BootStrap.groovy
@@ -1,7 +1,13 @@
 package org.grails.guides
 
+import groovy.transform.CompileStatic
+
+@CompileStatic
 class BootStrap {
 
+    MakeService makeService
+    ModelService modelService
+    VehicleService vehicleService
     def init = { servletContext ->
     }
     def destroy = {
